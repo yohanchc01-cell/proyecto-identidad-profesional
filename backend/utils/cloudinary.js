9cc0b28d863bb0adcfe206a4e12b6ec03615307b
Yohan Castro <yohanchc01@gmail.com>
2026-04-23 22:26:23 -0500

    add cloudinary multer

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "TU_CLOUD_NAME",
-  api_key: "TU_API_KEY",
-  api_secret: "TU_API_SECRET"
+  cloud_name: "dibwzbwwu",
+  api_key: "528297583792219",
+  api_secret: "kCrHkkaQWjOMoVO30M_Xc-pIz8Y"
 });
 
 module.exports = cloudinary;
