326a671ad1555f22d710a9b3314a1bdd7cb7f796
Yohan Castro <yohanchc01@gmail.com>
2026-04-25 18:55:11 -0500

    fix: Persistencia de fotoUrl en backend

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -60,7 +60,8 @@ router.post("/login", async (req, res) => {
       nombre: user.nombre,
       email: user.email,
       universidad: user.universidad,
-      carrera: user.carrera
+      carrera: user.carrera,
+      fotoUrl: user.fotoUrl
     }
   });
 });
@@ -76,7 +77,7 @@ router.delete("/user/:id", async (req, res) => {
 });
 
 router.put("/user/:id", async (req, res) => {
-  const { nombre, email, documento, universidad, carrera } = req.body;
+  const { nombre, email, documento, universidad, carrera, fotoUrl } = req.body;
   
   try {
     const user = await User.findById(req.params.id);
@@ -87,6 +88,7 @@ router.put("/user/:id", async (req, res) => {
     user.documento = documento || user.documento;
     user.universidad = universidad || user.universidad;
     user.carrera = carrera || user.carrera;
+    user.fotoUrl = fotoUrl || user.fotoUrl;
 
     await user.save();
     
